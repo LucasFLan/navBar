815de33b292cab25e1c24fe918b71eea25b7b7f8
Lucas <lucaslan10@hotmail.com>
2023-08-27 22:01:18 -0300

    NavBar

---
+++ main.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body')
+const sidebar = body.querySelector('.sidebar')
+const toggle = body.querySelector('.toggle')
+const searchBtn = body.querySelector('.search-box')
+const modeSwitch = body.querySelector('.toggle-switch')
+const modeText = body.querySelector('.mode-text');
+
+
+toggle.addEventListener('click', (e) => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener('click', (e) => {
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener('click', (e) => {
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
+})
+
